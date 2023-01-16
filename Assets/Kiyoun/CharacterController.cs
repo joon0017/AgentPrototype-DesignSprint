cab40c46fd08234858a8602e6c78db1af6f23824
kiyounkim <21900072@handong.ac.kr>
2023-01-16 14:40:35 +0900

    Paladin Animation Finished
    기본 전사 애니매이션 완료.

--- Assets/Kiyoun/CharacterController.cs
+++ Assets/Kiyoun/CharacterController.cs
@@ -7,7 +7,7 @@ public class CharacterController : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
-
+    bool canMove=true;
     Vector3 moveVec;
     Animator anim;
     // Start is called before the first frame update
@@ -19,11 +19,23 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hAxis = Input.GetAxisRaw("Horizontal");    
-        vAxis = Input.GetAxisRaw("Vertical");
-        moveVec = new Vector3(hAxis,0,vAxis).normalized;
-        transform.position += moveVec * speed * Time.deltaTime;
-        transform.LookAt(transform.position + moveVec);
-        anim.SetBool("Moving",moveVec != Vector3.zero);
+        if(canMove){
+            hAxis = Input.GetAxisRaw("Horizontal");    
+            vAxis = Input.GetAxisRaw("Vertical");
+            moveVec = new Vector3(hAxis,0,vAxis).normalized;
+            transform.position += moveVec * speed * Time.deltaTime;
+            transform.LookAt(transform.position + moveVec);
+            anim.SetBool("Moving",moveVec != Vector3.zero);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Attack");
+        }
+    }
+    public void StopMove(){
+        canMove=false;
+    }
+    public void StartMove(){
+        canMove=true;
     }
 }
