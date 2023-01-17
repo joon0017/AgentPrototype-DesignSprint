2399ebc268e721fb8b1b32369c39e50ceb0a2bc5
Luvxy <93637069+Luvxy@users.noreply.github.com>
2023-01-17 11:35:58 +0900

    Fight Scene 적 랜덤 생성

---
+++ Assets/Subin/Script/GenerateMap.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMap : MonoBehaviour
+{
+    public static GenerateMap Instance;
+    public int EnemyNum;
+    public GameObject Enemy;
+
+    Queue<Target1> poolingObjectQueue = new Queue<Target1>();
+
+    void Awake()
+    {
+        Instance = this;
+        Initialize(10);
+    }
+
+    void Start()
+    {
+        for(int i=0; i<EnemyNum; i++)
+        {
+            var obj = GetObject();
+            obj.transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        }
+    }
+
+    private void Initialize(int EnemyNum)
+    {
+        for(int i = 0; i < EnemyNum; i++)
+        {
+            poolingObjectQueue.Enqueue(CreateNewObject());
+            
+        }
+    }
+
+    private Target1 CreateNewObject()
+    {
+        var newObj = Instantiate(Enemy).GetComponent<Target1>();
+        newObj.gameObject.SetActive(false);
+        newObj.transform.SetParent(transform);
+        return newObj;
+    }
+
+    public static Target1 GetObject()
+    {
+        if(Instance.poolingObjectQueue.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newObj = Instance.CreateNewObject();
+            newObj.gameObject.SetActive(true);
+            newObj.transform.SetParent(null);
+            return newObj;
+        }
+    }
+
+    public static void ReturnObject(Target1 obj)
+    {
+        obj.gameObject.SetActive(false);
+        obj.transform.SetParent(Instance.transform);
+        Instance.poolingObjectQueue.Enqueue(obj);
+    }
+}
---
+++ Assets/Subin/Target1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target1 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag=="AttackArea")
+            GenerateMap.ReturnObject(this);
+    }
+
+    void OnCollisionStay(Collision coll) { 
+        Debug.Log(coll.gameObject.name);
+
+        if(coll.gameObject.tag=="AttackArea")
+            GenerateMap.ReturnObject(this);
+        
+        if(coll.gameObject.name == "Plane")
+        {
+            Debug.Log("Plane");
+            return;
+        }
+
+        if(coll.gameObject.name == "Player")
+        {
+            Debug.Log("Player");
+            return;
+        }
+
+        this.transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+    }
+}
