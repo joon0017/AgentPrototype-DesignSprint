808da58b1ba85b3cd4ba71b107a8ba0217b6e29f
Sniree <97450065+joon0017@users.noreply.github.com>
2023-01-17 15:10:55 +0900

    target1 edit

--- Assets/Subin/Script/Target1.cs
+++ Assets/Subin/Script/Target1.cs
@@ -21,24 +21,13 @@ public class Target1 : MonoBehaviour
     }
 
     void OnCollisionStay(Collision coll) { 
-        Debug.Log(coll.gameObject.name);
+        // Debug.Log(coll.gameObject.name);
 
-        if(coll.gameObject.tag=="AttackArea")
-            GenerateMap.ReturnObject(this);
-        
-        if(coll.gameObject.name == "Plane")
-        {
-            Debug.Log("Plane");
+        if (coll.gameObject.CompareTag("Wall") || coll.gameObject.CompareTag("Trap")){
+            this.transform.position = new Vector3(Random.Range(-10, 10), transform.position.y, Random.Range(-10, 10));
             return;
         }
-
-        if(coll.gameObject.name == "Player")
-        {
-            Debug.Log("Player");
-            return;
-        }
-
-        this.transform.position = new Vector3(Random.Range(-10, 10), transform.position.y, Random.Range(-10, 10));
+        // this.transform.position = new Vector3(Random.Range(-10, 10), transform.position.y, Random.Range(-10, 10));
     }
 
     public void OnDamage()
