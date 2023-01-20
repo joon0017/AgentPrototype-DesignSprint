0f61587746cfec64d74da7907e7bedcba105637c
finess0413 <105401802+finess0413@users.noreply.github.com>
2023-01-20 13:46:39 +0900

    Update GameStratSystem.cs

--- Assets/Subin/Script/GameStratSystem.cs
+++ Assets/Subin/Script/GameStratSystem.cs
@@ -8,7 +8,7 @@ public class GameStratSystem : MonoBehaviour
 
     private void Update(){
         if(Input.anyKeyDown){
-            PlayerPrefs.SetInt("Tutorial",3);
+            PlayerPrefs.SetInt("Tutorial",1);
             SceneManager.LoadScene("SelectChar");
         }
     }
