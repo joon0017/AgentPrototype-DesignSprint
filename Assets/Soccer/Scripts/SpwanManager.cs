15df7f2bd2c8e56d8de77e7be56ecd71cc56eb77
inbaea <49175916+inbaea@users.noreply.github.com>
2023-02-27 11:02:06 +0900

    update

--- Assets/Soccer/Scripts/SpwanManager.cs
+++ Assets/Soccer/Scripts/SpwanManager.cs
@@ -6,35 +6,43 @@ public class SpwanManager : MonoBehaviour
 {
     public GameObject gameManager;
     public GameObject[] agentPrefabs;
+    public GameObject soccerTwosScript;
 
     public void Start()
     {
         gameManager = GameObject.Find("GameManager");
+        soccerTwosScript = GameObject.Find("SoccerFieldTwos");
 
         if (gameManager.GetComponent<GameManager>().selectedCharacter[0] == "Balance_Agent")
         {
             GameObject agent = Instantiate(agentPrefabs[0], new Vector3(-3.19f, 0.5f, -1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[0] = agent;
         }
         else if (gameManager.GetComponent<GameManager>().selectedCharacter[0] == "Striker_Agent")
         {
             GameObject agent = Instantiate(agentPrefabs[1], new Vector3(-3.19f, 0.5f, -1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[0] = agent;
         }
         else if (gameManager.GetComponent<GameManager>().selectedCharacter[0] == "Goalkeeper_Agent")
         {
             GameObject agent = Instantiate(agentPrefabs[2], new Vector3(-3.19f, 0.5f, -1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[0] = agent;
         }
 
         if (gameManager.GetComponent<GameManager>().selectedCharacter[1] == "Balance_Agent")
         {
-            GameObject agent = Instantiate(agentPrefabs[0], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            GameObject agent1 = Instantiate(agentPrefabs[0], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[1] = agent1;
         }
         else if (gameManager.GetComponent<GameManager>().selectedCharacter[1] == "Striker_Agent")
         {
-            GameObject agent = Instantiate(agentPrefabs[1], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            GameObject agent1 = Instantiate(agentPrefabs[1], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[1] = agent1;
         }
         else if (gameManager.GetComponent<GameManager>().selectedCharacter[1] == "Goalkeeper_Agent")
         {
-            GameObject agent = Instantiate(agentPrefabs[2], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            GameObject agent1 = Instantiate(agentPrefabs[2], new Vector3(-3.19f, 0.5f, 1.2f), Quaternion.identity);
+            //soccerTwosScript.GetComponent<SoccerEnvController>().AgentsList[1] = agent1;
         }
     }
 }
